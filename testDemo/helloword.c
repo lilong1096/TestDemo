f51ac784485aa8265b747c74783559b1da853235
lilong <m13657310961@163.com>
2017-02-24 16:43:44 +0800

    I add a print .....

--- testDemo/helloword.c
+++ testDemo/helloword.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main()
 {
+
+    printf("modify the helloword.c to testing...");
 	printf("this is a demo to discript how to use sourcetree");
 	return 0;
 }
